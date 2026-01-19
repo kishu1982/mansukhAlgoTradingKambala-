19742ad6db1e4252471b3e98e608c816caed5eca
kishu_chopra <158042875+kishu1982@users.noreply.github.com>
2026-01-19 11:21:06 +0530

    dataBase dynamic name added for adding account code in end of database name

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -2,11 +2,22 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
+const baseDbName = process.env.MONGO_DB_NAME;
+const norenAccId = process.env.NOREN_ACC_ID;
+
+
+if (!baseDbName || !norenAccId) {
+  throw new Error(
+    'MONGO_DB_NAME or NOREN_ACC_ID is missing in environment variables',
+  );
+}
 
 export const databaseConfig: TypeOrmModuleOptions = {
   type: 'mongodb',
   url: process.env.MONGO_URI,
-  database: process.env.MONGO_DB_NAME,
+  //database: process.env.MONGO_DB_NAME,
+  // ✅ dynamic database name
+  database: `${baseDbName}_${norenAccId}`,
 
   autoLoadEntities: true,
   synchronize: false,
