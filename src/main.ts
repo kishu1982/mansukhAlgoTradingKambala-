50a6cc673274ae0e86679c8a31f979eecffc5d63
kishu_chopra <158042875+kishu1982@users.noreply.github.com>
2026-02-09 13:34:33 +0530

    base settingt : added cors origin to all

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { ExpressAdapter } from '@nestjs/platform-express';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // IMPORTANT: enables correct client IP from x-forwarded-for
-  const expressApp =app.getHttpAdapter().getInstance();
+  const expressApp = app.getHttpAdapter().getInstance();
   expressApp.set('trust proxy', true);
 
   app.useGlobalPipes(
@@ -19,6 +19,15 @@ async function bootstrap() {
       },
     }),
   ); // defining gloabally
+
+  // fixing cors for nextjs frontend
+  app.enableCors({
+    origin: true, // your NextJS frontend alows all origins, you can specify your frontend URL here
+    // origin: 'http://localhost:3001', // your NextJS frontend
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // if using cookies/auth
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
